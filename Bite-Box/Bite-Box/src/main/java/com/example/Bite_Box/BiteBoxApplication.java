1b712f307f1047c4e6a26cbaee3cd8ad69a7b8fd
Aadavan123 <136162790+Aadavan123@users.noreply.github.com>
2025-03-21 15:46:13 +0530

    Add files via upload

---
+++ Bite-Box/Bite-Box/src/main/java/com/example/Bite_Box/BiteBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bite_Box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteBoxApplication.class, args);
+	}
+
+}
---
+++ Bite-Box/Bite-Box/src/test/java/com/example/Bite_Box/BiteBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bite_Box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
